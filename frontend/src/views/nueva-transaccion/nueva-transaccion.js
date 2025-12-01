905487906a65d39562ac734be2cfe48b78165f36
Franco-sigm <francoherrera.30@outlook.com>
2025-11-30 22:02:54 -0300

    Genero sección nueva transacción y conecto formulario al botón

---
+++ frontend/src/views/nueva-transaccion/nueva-transaccion.js
@@ -0,0 +1,7 @@
+document.addEventListener('DOMContentLoaded', () => {
+    // Referencias al DOM (Layout)
+    const tituloSeccion = document.getElementById('comunidad-nombre-titulo');
+    const linkDashboard = document.getElementById('link-dashboard');
+    const linkResidentes = document.getElementById('link-residentes');
+    const linkPagosGastos = document.getElementById('link-pagos-gastos');
+});
