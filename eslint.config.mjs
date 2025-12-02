54e26a09ecb8258a43924cfbacc3f29652523e75
Franco-sigm <francoherrera.30@outlook.com>
2025-12-02 10:41:22 -0300

    se impmenta Eslint y se definen reglas basicas para garantizar buenas practicas

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -3,5 +3,16 @@ import globals from "globals";
 import { defineConfig } from "eslint/config";
 
 export default defineConfig([
-  { files: ["**/*.{js,mjs,cjs}"], plugins: { js }, extends: ["js/recommended"], languageOptions: { globals: globals.browser } },
+  {
+    files: ["**/*.{js,mjs,cjs}"],
+    plugins: { js },
+    extends: ["js/recommended"],
+    languageOptions: { globals: globals.browser },
+    rules: {
+      "semi": ["error", "always"],        // Obliga a usar punto y coma
+      "quotes": ["error", "double"],      // Obliga a usar comillas dobles
+      "no-unused-vars": "warn",           // Advierte variables no usadas
+      "no-console": "warn"                // Marca console.log como advertencia
+    }
+  },
 ]);
